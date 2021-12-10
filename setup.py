b27c787dc1ec0ce3be00ff01834ef710e1aac6d1
sileod <damien.sileo@gmail.com>
2021-12-10 11:34:59 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='metaeval',
+      version='0.1',
+      description='Aggregation of datasets for metalearning',
+      url='https://github.com/sileod/metaeval',
+      author='sileod',
+      license='MIT',
+      install_requires=['datasets','pandas'],
+      download_url='https://github.com/sileod/metaeval/archive/refs/tags/v0.tar.gz',
+      py_modules=['metaeval'],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+      zip_safe=False)
