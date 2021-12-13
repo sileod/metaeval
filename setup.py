e34492d97a9cbc53fca41533d964f7baa9cfe073
sileod <damien.sileo@gmail.com>
2021-12-13 13:06:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-long_description = (this_directory / "README.md").read_text()
+# long_description = (this_directory / "README.md").read_text()
 
 setup(name='metaeval',
       version='0.1',
